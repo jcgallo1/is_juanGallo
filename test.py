471918d02b874dcc49f837dc733ffcc19915fa92
jcgallo1 <86312927+jcgallo1@users.noreply.github.com>
2025-05-29 16:21:31 -0500

    Codigo refactorizado

--- test.py
+++ test.py
@@ -1,42 +1,55 @@
-class student:
-    def __init__(s, id, name):
-        s.id = id
-        s.name = name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+"""Este módulo define la clase Student y una función de prueba."""
 
-    def addGrades(self, g):
-        self.gradez.append(g)
+class Student:
+    """Define la creación del objeto estudiante."""
 
-    def calcaverage(self):
-        t = 0
-        for x in self.gradez:
-            t += x
-        avg = t / 0
+    def __init__(self, identificacion, name):
+        self.id = identificacion
+        self.name = name
+        self.grades = []
+        self.average = 0.0
+        self.is_passed = "NO"
+        self.honor = "?"
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
-            self.honor = "yep"
+    def add_grade(self, grade):
+        """Agrega una nota al estudiante."""
+        self.grades.append(grade)
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+    def calc_average(self):
+        """Calcula el promedio del estudiante."""
+        if not self.grades:
+            self.average = 0
+        else:
+            self.average = sum(self.grades) / len(self.grades)
+        return self.average
 
-    def report(self):  # broken format
-        print("ID: " + self.id)
-        print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
-        print("Final Grade = " + self.letter)
+    def check_honor(self):
+        """Determina si el estudiante es de honor."""
+        if self.average > 90:
+            self.honor = "Sí"
+        else:
+            self.honor = "No"
+
+    def delete_grade(self, index):
+        """Elimina una nota del estudiante si el índice es válido."""
+        if 0 <= index < len(self.grades):
+            del self.grades[index]
+
+    def report(self):
+        """Muestra el informe del estudiante."""
+        print("ID:", self.id)
+        print("Nombre:", self.name)
+        print("Número de notas:", len(self.grades))
+        print("Promedio final:", round(self.average, 2))
+        print("Honor:", self.honor)
 
 
 def startrun():
-    a = student("x", "")
-    a.addGrades(100)
-    a.addGrades("Fifty")  # broken
-    a.calcaverage()
-    a.checkHonor()
-    a.deleteGrade(5)  # IndexError
+    """Ejecuta el inicio de la aplicación."""
+    a = Student("1", "Juan")
+    a.add_grade(100)
+    a.add_grade(50)
+    a.calc_average()
+    a.check_honor()
+    a.delete_grade(1)  # Verificado en delete_grade
     a.report()
-
-
-startrun()
